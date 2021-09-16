b740eba865f79fda28d7697ba74ff8f3023fb7c1
Alexei Lyashenko <lyashenko1alexei@gmail.com>
2021-09-16 15:59:58 +0300

    feat: add adaptive, scroll-anchors

--- script/script.js
+++ script/script.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const burgerBtn = document.querySelector('.header__burger'),
-      headerList = document.querySelector('.header__navigation');
+      headerList = document.querySelector('.header__navigation'),
+      links = document.querySelectorAll('.header__navigation > ul > li > a'),
+      anchors = document.querySelectorAll('h3.--title');
 
 const toggle = (target, classChange) => {
     target.classList.toggle(classChange);
@@ -13,9 +15,31 @@ const removeClass = (target, classChange) => {
     target.classList.remove(classChange);
 }
 
+const scroll = (target) => {
+    target.scrollIntoView(
+        {
+            block: "center",
+            behavior: "smooth"
+        }
+    )
+}
+
 burgerBtn.addEventListener('click', () => {
     toggle(burgerBtn, 'active-burger');
     toggle(headerList, 'active-list');
-    toggle(document.body, 'overflow-hidden');
     toggle(document.querySelector('html'), 'overflow-hidden');
 })
+    
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', (e) => {
+        e.preventDefault()
+        if(!burgerBtn.classList.contains('.active-burger')) {
+            removeClass(burgerBtn, 'active-burger')
+            removeClass(headerList, 'active-list')
+            removeClass(document.querySelector('html'), 'overflow-hidden');
+            scroll(anchors[i])
+        }
+        scroll(anchors[i])
+    })
+}
+
